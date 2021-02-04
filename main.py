f4629fac02e9ef7686fb3095598ee5968c9f2707
HHR <i@huhaorui.com>
2021-02-04 14:34:26 +0800

    update

--- main.py
+++ main.py
@@ -30,12 +30,28 @@ def sendmail(subject, message, config):
 
 
 def get_gpa(msg):
-    return 5  # TODO 实现GPA计算
+    gp = 0
+    credit = 0
+    for item in msg:
+        if item[6] != "任选课":
+            gp = gp + item[4] * item[5]
+            credit = credit + item[4]
+    if credit != 0:
+        return gp / credit
+    else:
+        return 0
+
+
+def beautify_msg(msg):
+    for item in msg:
+        item[1] = "{0}({1}%)".format(item[1], 100 - item[7])
+        item[2] = "{0}({1}%)".format(item[2], item[7])
+    return msg
 
 
 def get_HTML(msg):
     rows = []
-    for score in msg:
+    for score in beautify_msg(msg):
         row = "<tr><td>{0}</td><td>{1}</td><td>{2}</td><td>{3}</tr>".format(score[0], score[1], score[2], score[3])
         rows.append(row)
     gpa = get_gpa(msg)
@@ -76,39 +92,44 @@ def error(msg):
     print(msg)
 
 
-def get_score_detail(cfg):  # 返回成绩信息的res
-
+def get_json(url):
     headers = {
         "Host": "api.jh.zjut.edu.cn",
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
                       "Chrome/79.0.3945.88 Safari/537.36 Edg/79.0.309.54 "
     }
 
-    url = "http://api.jh.zjut.edu.cn/student/scoresDetailZf.php?ip=164&username={0}&password={1}&year={2}&term={3}". \
-        format(cfg[0], cfg[1], cfg[2], cfg[3])
     content = requests.get(url, headers=headers).content.decode().encode("GBK")
-    decodedJSON = json.loads(content)
-    while decodedJSON["status"] != "success":
+    result = json.loads(content)
+    while result["status"] != "success":
         error('查询错误')
         time.sleep(30)
         content = requests.get(url, headers=headers).content.decode().encode("GBK")
-        decodedJSON = json.loads(content)
+        result = json.loads(content)
+    return result
+
+
+def get_score_detail(cfg):  # 返回成绩信息的res
+    url = "http://api.jh.zjut.edu.cn/student/scoresDetailZf.php?ip=164&username={0}&password={1}&year={2}&term={3}". \
+        format(cfg[0], cfg[1], cfg[2], cfg[3])
+    score_detail = get_json(url)
     score_summary = {}
     value = {}
-    for each in decodedJSON["msg"]:
+    for each in score_detail["msg"]:
         if not (score_summary.__contains__(each['kcmc'])):
             value.clear()
         value[each['xmblmc']] = each['xmcj']
         score_summary[each['kcmc']] = copy.deepcopy(value)
     score_list = []
     for k in score_summary.items():
-        score_item = [0, 0, 0, 0, 0, 0, 0]  # 课程名称，平时成绩，期末成绩，总评，学分，学分绩点，是否计入
+        score_item = [0, 0, 0, 0, 0, 0, 0, 0]  # 课程名称，平时成绩，期末成绩，总评，学分，学分绩点，是否计入,期末考占比
         score_item[0] = k[0]
         for score in k[1].items():
             if "平时" in score[0]:
                 score_item[1] = score[1]
             if "期末" in score[0]:
                 score_item[2] = score[1]
+                score_item[7] = eval(score[0].split("(")[1].split("%")[0])
             if "总评" in score[0]:
                 score_item[3] = score[1]
         score_list.append(score_item)
@@ -116,19 +137,16 @@ def get_score_detail(cfg):  # 返回成绩信息的res
 
 
 def get_gpa_info(cfg, res):  # 为res添加GPA信息
-    # '''
-    # for each in decodedJSON["msg"]:
-    #     if each['kcxzmc'] != '任选课':
-    #         credit = credit + eval(each['xf'])
-    #         GP = GP + eval(each['xf']) * eval(each['jd'])
-    #     msg = msg + each['kcmc'] + '\t' + each['classscore'] + '\n'
-    # GPA = GP / credit
-    # msg = msg + "当前GPA：\t" + str(GPA) + '\n'
-    # print("yes")
-    # #sendmail(msg, config)
-    # size = len(decodedJSON["msg"])
-    # '''
-    return res  # TODO 实现功能
+    url = "http://api.jh.zjut.edu.cn/student/scoresZf.php?ip=164&username={0}&password={1}&year={2}&term={3}". \
+        format(cfg[0], cfg[1], cfg[2], cfg[3])
+    score_detail = get_json(url)
+    for score in score_detail["msg"]:
+        for item in res:
+            if item[0] == score["kcmc"]:
+                item[4] = eval(score["xf"])
+                item[5] = eval(score["jd"])
+                item[6] = score["kcxzmc"]
+    return res
 
 
 def check_password(cfg):  # 判断密码是否正确，正确返回True
@@ -137,7 +155,6 @@ def check_password(cfg):  # 判断密码是否正确，正确返回True
 
 def get_config():
     config_file = open('config.ini', mode='r', encoding='utf-8')
-
     return config_file.read().split('\n')
 
 
@@ -155,7 +172,8 @@ if __name__ == '__main__':
             new_score_list = get_score_detail(config)
             if len(new_score_list) != size:  # 出新成绩了
                 scores = get_gpa_info(config, new_score_list)  # 加上GPA信息
-                sendmail("出成绩了！", get_HTML(scores), config)
+                html = get_HTML(scores)
+                sendmail("出成绩了！", html, config)
         except requests.exceptions.ConnectionError as e:
             error('Network Error')
         except KeyError as e:
